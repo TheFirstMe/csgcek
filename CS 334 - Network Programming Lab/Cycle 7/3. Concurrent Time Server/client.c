223291146901e7573d16ad7953613b4cc90dd705
Sourabh <sourabhsubhod@gmail.com>
2019-04-16 00:43:31 +0530

    Cycle 7 Q3

---
+++ CS 334 - Network Programming Lab/Cycle 7/3. Concurrent Time Server/client.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+#include<unistd.h>
+#define PORT 8888
+
+void error(char *msg){
+    perror(msg);
+    exit(0);
+}
+
+int main(){
+    int sockfd;
+    char buffer[256];
+    struct sockaddr_in serv_addr;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0)
+        error("Error creating socket");
+    memset(&serv_addr,0,sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+    int servsize = sizeof(serv_addr);
+    if(connect(sockfd, (struct sockaddr *) &serv_addr, servsize) < 0){
+        error("error connecting");
+    }
+
+    strcpy(buffer,"TIME");
+    send(sockfd,buffer,sizeof(buffer),0);
+    memset(buffer,0,sizeof(buffer));
+    recv(sockfd,buffer,sizeof(buffer),0);
+    printf("\nServer time: %s",buffer);
+    // close(sockfd);
+    return 0;
+}
---
+++ CS 334 - Network Programming Lab/Cycle 7/3. Concurrent Time Server/server.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<pthread.h>
+
+#define PORT 8888
+// int main(){
+//     time_t t;
+//     time(&t);
+//     printf("%s",ctime(&t));
+//     return 0;
+// }
+void error(char *msg){
+    perror(msg);
+    exit(0);
+}
+
+void *connection_handler(void *);
+
+int main(){
+    int sockfd,newsockfd,clilen;
+    struct sockaddr_in serv_addr,cli_addr;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0){
+		error("Failed to open socket!!");
+    }
+    int enable = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+        error("setsockopt(SO_REUSEADDR) failed");
+    memset(&serv_addr,0,sizeof(serv_addr));
+    memset(&cli_addr,0,sizeof(cli_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+
+    if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+        error("Error binding!!");
+    }
+    listen(sockfd,3);
+    clilen = sizeof(cli_addr);
+    int *sockptr;
+    while(newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)){
+        pthread_t sniffer_thread;
+        if(pthread_create(&sniffer_thread, NULL, connection_handler, &newsockfd) < 0){
+            error("cannot create thread");
+        }
+    }
+    if(newsockfd<0){
+        error("accept failed");
+    }
+    return 0;
+}
+
+void *connection_handler(void *socket_descriptor){
+    int sockfd = *(int *) socket_descriptor;
+    char buffer[256];
+    if(recv(sockfd, buffer, sizeof(buffer),0) < 0){
+        error("cannot receive");
+    }
+    memset(buffer,0,sizeof(buffer));
+    time_t t;
+    time(&t);
+    strcpy(buffer,ctime(&t));
+    if(send(sockfd, buffer, sizeof(buffer),0) < 0){
+        error("cannot send");
+    }
+    close(sockfd);
+}
